64e6233c2f56da66d34f45146a919c218ff5f8fb
Nitin Reddy <reddynitin3@gmail.com>
2020-02-25 11:21:26 +0530

    Creates todo items and saves to the db

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <img alt="Vue Logo" src="@/assets/logo.png" id="vue-logo" />
+    <div class="title">What I am doing?</div>
+    <input v-model="myTodo" />
+    <button @click="addToDo">Add</button>
+    <div v-if="errors !== ''" id="errors">
+      {{errors}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import { db } from '@/main';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => {
+    return {
+      myTodo: '',
+      errors: ''
+    };
+  },
+  methods: {
+    addToDo: function(){
+      this.errors = '';
+
+      if(this.myTodo !== '') {
+        db.collection('items').add({
+          title: this.myTodo,
+          createdAt: Date.now()
+        }).then(response => {
+          if(response) {
+            this.myTodo = ''
+          }
+        }).catch(error => {
+          this.errors = error;
+        })
+      } else {
+        this.errors = 'Enter a todo';
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
+body,
+html {
+  background: #8ac8e5;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.home {
+  width: 300px;
+  margin: auto;
+}
+
+#vue-logo {
+  width: 100px;
+}
+
+input,
+button {
+  border: 0;
+  width: 100%;
+  padding: 7px;
+}
+
+input {
+  font-size: 12px;
+  margin: 0 0 10px;
+}
+
+button {
+  background: #000;
+  color: #fff;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+#errors {
+  background:  red;
+  margin: 10px 0;
+  padding: 5px;
+  color: #fff;
+  text-align: left;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './router';
+// import store from './store';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: "AIzaSyAH5eNQC_SrL5xhKsAJtrkY083XrQuxdwE",
+  authDomain: "vue-learning-track.firebaseapp.com",
+  databaseURL: "https://vue-learning-track.firebaseio.com",
+  projectId: "vue-learning-track",
+  storageBucket: "vue-learning-track.appspot.com",
+  messagingSenderId: "305624742426",
+  appId: "1:305624742426:web:634360e9ff4027e7cc2fa3",
+  measurementId: "G-K0QJ7DDQ1X"
+};
+
+firebase.initializeApp(config);
+
+export const db = firebase.firestore();
 new Vue({
   render: h => h(App),
 }).$mount('#app')
